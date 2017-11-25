8b9a00f1b5a51f67b2d9eca7f21ebc15c6eca6d5
Yevhenii Shchemelinin <yevgenii1988@gmail.com>
2017-11-25 13:03:30 +0200

    repository config

--- src/main/java/com/cashmachine/config/RepositoryConfig.java
+++ src/main/java/com/cashmachine/config/RepositoryConfig.java
@@ -6,9 +6,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate3.HibernateTemplate;
+import org.springframework.orm.hibernate3.HibernateTransactionManager;
 import org.springframework.orm.hibernate3.annotation.AnnotationSessionFactoryBean;
-import org.springframework.orm.hibernate5.HibernateTemplate;
-import org.springframework.orm.hibernate5.HibernateTransactionManager;
+
 
 import javax.sql.DataSource;
 import java.util.Properties;
